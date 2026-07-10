f27af9d109c14892c27742824878440f7f254f0a
Shi_ni <devengrg6@gmail.com>
2026-07-10 21:47:34 +0545

    Update gcs_interactive_win.py

--- gcs_interactive_win.py
+++ gcs_interactive_win.py
@@ -25,7 +25,7 @@ except ImportError:
     print("keyboard library not found. Run: pip install keyboard")
     sys.exit(1)
 
-DEVICE_B_IP = "192.168.1.145"
+DEVICE_B_IP = "192.168.1.1
 UAV_LISTEN_PORT = 14550
 
 def main():
